65d5580f0bfdf0960a59c1c068760aaf9204f900
Steve Bennett <me@stevebennett.me>
2025-03-04 23:18:39 +1100

    pkg

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import {
     curveToCoords,
     regularizeMidpoints,
     curveToGeoJSON,
-} from './src/wktCurves';
-import wktToCurve from './src/wktToCurve';
-import curveToWkt from './src/curveToWkt';
+} from './src/wktCurves.js';
+import wktToCurve from './src/wktToCurve.js';
+import curveToWkt from './src/curveToWkt.js';
 
 export {
     arcFromPoints,
